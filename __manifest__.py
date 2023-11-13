76bc85b5fbd36ac830cba28bb44514ba14a0e557
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-11-13 13:59:01 +0100

    Basic odoo module structure

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Shermine Module',
+    'version': '1.0.1',
+    'author': 'Charlie Rostant YOSSA',
+    'Website': 'sherminemodule.sample',
+    'summary': 'Just a litle module',
+    'descriptio': 'Made just for test pipeline',
+    'licence': 'AGPL-3'
+}
