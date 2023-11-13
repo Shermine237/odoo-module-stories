218fd671579d40534f673594d05211e41cf5154a
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-11-13 18:30:23 +0100

    Add website link

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,6 @@
     'name': 'Users Stories',
     'version': '1.0.1',
     'author': 'Charlie Rostant YOSSA',
-    'Website': 'sherminemodule.sample',
+    'website': 'sherminemodule.sample',
     'description': 'User Stories'
 }
